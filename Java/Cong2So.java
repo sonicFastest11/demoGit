fea15300f884de9e342d1436d2e8271d93ae7716
Your Name <dohongson6698@gmail.com>
2019-07-02 11:21:28 +0700

    son edit

---
+++ Java/Cong2So.java
@@ -0,0 +1,15 @@
+﻿package ArrayListEx;
+son
+import java.util.Scanner;
+//Đề bài: Cho 2 số nguyên a, b. Tính tổng 2 số đó biết (|a| và |b| <= 10 tỷ ).
+//Đầu vào: Một dòng chứa 2 số cách nhau bởi dấu cách.
+//Đầu ra: Một số duy nhất là tổng của 2 số.
+// gio e can them dieu kien kia la oke
+public class Cong2So {
+	public static void main(String[] args) {
+  		Scanner scan = new Scanner(System.in);
+	  	int a = scan.nextInt();
+	  	int b = scan.nextInt();
+	   System.out.print(a+b);
+	 }
+}
